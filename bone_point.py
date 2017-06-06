51bcaffc547eed16d69338a2871c54e9b6facc34
JesseYang <jesse.yang1985@gmail.com>
2017-06-06 15:43:39 +0800

    first commit

---
+++ bone_point.py
@@ -0,0 +1,202 @@
+import cv2
+import sys
+import argparse
+import numpy as np
+import os
+import multiprocessing
+
+import tensorflow as tf
+from tensorflow.contrib.layers import variance_scaling_initializer
+from tensorpack import *
+from tensorpack.utils.stats import RatioCounter
+from tensorpack.tfutils.symbolic_functions import *
+from tensorpack.tfutils.summary import *
+from reader import *
+from cfgs.config import cfg
+
+TOTAL_BATCH_SIZE = 8
+INPUT_SHAPE = 224
+
+
+class Model(ModelDesc):
+    def __init__(self, depth):
+        super(Model, self).__init__()
+        self.depth = depth
+
+    def _get_inputs(self):
+        return [InputDesc(tf.uint8, [None, INPUT_SHAPE, INPUT_SHAPE, 3], 'input'),
+                InputDesc(tf.float32, [None, cfg.bone_num * 2], 'label')]
+
+    def _build_graph(self, inputs):
+        image, label = inputs
+        image = tf.cast(image, tf.float32) * (1.0 / 255)
+
+        # Wrong mean/std are used for compatibility with pre-trained models.
+        # Should actually add a RGB-BGR conversion here.
+        image_mean = tf.constant([0.485, 0.456, 0.406], dtype=tf.float32)
+        image_std = tf.constant([0.229, 0.224, 0.225], dtype=tf.float32)
+        image = (image - image_mean) / image_std
+        image = tf.transpose(image, [0, 3, 1, 2])
+
+        def shortcut(l, n_in, n_out, stride):
+            if n_in != n_out:
+                return Conv2D('convshortcut', l, n_out, 1, stride=stride)
+            else:
+                return l
+
+        def basicblock(l, ch_out, stride, preact):
+            ch_in = l.get_shape().as_list()[1]
+            if preact == 'both_preact':
+                l = BNReLU('preact', l)
+                input = l
+            elif preact != 'no_preact':
+                input = l
+                l = BNReLU('preact', l)
+            else:
+                input = l
+            l = Conv2D('conv1', l, ch_out, 3, stride=stride, nl=BNReLU)
+            l = Conv2D('conv2', l, ch_out, 3)
+            return l + shortcut(input, ch_in, ch_out, stride)
+
+        def bottleneck(l, ch_out, stride, preact):
+            ch_in = l.get_shape().as_list()[1]
+            if preact == 'both_preact':
+                l = BNReLU('preact', l)
+                input = l
+            elif preact != 'no_preact':
+                input = l
+                l = BNReLU('preact', l)
+            else:
+                input = l
+            l = Conv2D('conv1', l, ch_out, 1, nl=BNReLU)
+            l = Conv2D('conv2', l, ch_out, 3, stride=stride, nl=BNReLU)
+            l = Conv2D('conv3', l, ch_out * 4, 1)
+            return l + shortcut(input, ch_in, ch_out * 4, stride)
+
+        def layer(l, layername, block_func, features, count, stride, first=False):
+            with tf.variable_scope(layername):
+                with tf.variable_scope('block0'):
+                    l = block_func(l, features, stride,
+                                   'no_preact' if first else 'both_preact')
+                for i in range(1, count):
+                    with tf.variable_scope('block{}'.format(i)):
+                        l = block_func(l, features, 1, 'default')
+                return l
+
+        res_cfg = {
+            18: ([2, 2, 2, 2], basicblock),
+            34: ([3, 4, 6, 3], basicblock),
+            50: ([3, 4, 6, 3], bottleneck),
+            101: ([3, 4, 23, 3], bottleneck)
+        }
+        defs, block_func = res_cfg[self.depth]
+
+        with argscope(Conv2D, nl=tf.identity, use_bias=False,
+                      W_init=variance_scaling_initializer(mode='FAN_OUT')), \
+                argscope([Conv2D, MaxPooling, GlobalAvgPooling, BatchNorm], data_format='NCHW'):
+            logits = (LinearWrap(image)
+                      .Conv2D('conv0', 64, 7, stride=2, nl=BNReLU)
+                      .MaxPooling('pool0', shape=3, stride=2, padding='SAME')
+                      .apply(layer, 'group0', block_func, 64, defs[0], 1, first=True)
+                      .apply(layer, 'group1', block_func, 128, defs[1], 2)
+                      .apply(layer, 'group2', block_func, 256, defs[2], 2)
+                      .apply(layer, 'group3', block_func, 512, defs[3], 2)
+                      .BNReLU('bnlast')
+                      .GlobalAvgPooling('gap')
+                      .FullyConnected('linear-' + cfg.name, cfg.bone_num * 2, nl=tf.identity)())
+
+        logits = tf.sigmoid(logits) - 0.5
+        logits = tf.identity(logits, name='logits')
+
+        loss = tf.square(tf.subtract(logits, label))
+        loss = tf.reduce_mean(tf.reduce_sum(loss, 1), name='loss')
+
+        wd_cost = regularize_cost('.*/W', l2_regularizer(1e-4), name='l2_regularize_loss')
+        add_moving_summary(loss, wd_cost)
+        self.cost = tf.add_n([loss, wd_cost], name='cost')
+        # self.cost = tf.identity(loss, name='cost')
+
+    def _get_optimizer(self):
+        lr = get_scalar_var('learning_rate', 0.1, summary=True)
+        return tf.train.MomentumOptimizer(lr, 0.9, use_nesterov=True)
+
+
+def get_data(train_or_test):
+    # return FakeData([[64, 224,224,3],[64]], 1000, random=False, dtype='uint8')
+    isTrain = train_or_test == 'train'
+
+    if isTrain:
+        ds = Data(train_or_test)
+    else:
+        ds = Data(train_or_test)
+
+    if isTrain:
+        augmentors = [
+            imgaug.RandomOrderAug(
+                [imgaug.Brightness(30, clip=False),
+                 imgaug.Contrast((0.8, 1.2), clip=False),
+                 imgaug.Saturation(0.4),
+                 # rgb-bgr conversion
+                 imgaug.Lighting(0.1,
+                                 eigval=[0.2175, 0.0188, 0.0045][::-1],
+                                 eigvec=np.array(
+                                     [[-0.5675, 0.7192, 0.4009],
+                                      [-0.5808, -0.0045, -0.8140],
+                                      [-0.5836, -0.6948, 0.4203]],
+                                     dtype='float32')[::-1, ::-1]
+                                 )]),
+            imgaug.Clip(),
+            imgaug.ToUint8()
+        ]
+    else:
+        augmentors = [
+            imgaug.ToUint8()
+        ]
+    ds = AugmentImageComponent(ds, augmentors)
+    if isTrain:
+        ds = PrefetchDataZMQ(ds, min(20, multiprocessing.cpu_count()))
+    ds = BatchData(ds, BATCH_SIZE, remainder=not isTrain)
+    return ds
+
+
+def get_config(depth):
+    ds_train = get_data('train')
+    ds_test = get_data('test')
+
+    return TrainConfig(
+        dataflow=ds_train,
+        callbacks=[
+            ModelSaver(),
+            # InferenceRunner(dataset_val, [
+            #     ClassificationError('wrong-top1', 'val-error-top1'),
+            #     ClassificationError('wrong-top5', 'val-error-top5')]),
+            ScheduledHyperParamSetter('learning_rate',
+                                      [(0, 1e-2), (30, 1e-2)]),
+            HumanHyperParamSetter('learning_rate'),
+        ],
+        model=Model(depth),
+        max_epoch=500,
+    )
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--gpu', help='comma separated list of GPU(s) to use.', default='0')
+    parser.add_argument('--load', help='load model', default='ImageNet-ResNet18.npy')
+    parser.add_argument('-d', '--depth', help='resnet depth',
+                        type=int, default=18, choices=[18, 34, 50, 101])
+    args = parser.parse_args()
+
+    if args.gpu:
+        os.environ['CUDA_VISIBLE_DEVICES'] = args.gpu
+
+    assert args.gpu is not None, "Need to specify a list of gpu for training!"
+    NR_GPU = len(args.gpu.split(','))
+    BATCH_SIZE = TOTAL_BATCH_SIZE // NR_GPU
+
+    logger.auto_set_dir()
+    config = get_config(args.depth)
+    if args.load:
+        config.session_init = get_model_loader(args.load)
+    config.nr_tower = NR_GPU
+    SyncMultiGPUTrainer(config).train()
---
+++ reader.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# File: cifar.py
+# Author: Yuxin Wu <ppwwyyxx@gmail.com>
+#         Yukun Chen <cykustc@gmail.com>
+
+import os, sys
+import pickle
+import numpy as np
+from scipy import misc
+import random
+import six
+from six.moves import urllib, range
+import copy
+import logging
+import cv2
+from cfgs.config import cfg
+
+from tensorpack import *
+
+def get_data_list(fname):
+    with open(fname) as f:
+        content = f.readlines()
+    rec = [ele.split(' ') for ele in content]
+    return rec
+
+class Data(RNGDataFlow):
+    def __init__(self, train_or_test, shuffle=True):
+        """
+        Args:
+            train_or_test: string either 'train' or 'test'
+            shuffle: default to True
+        """
+        assert train_or_test in ['train', 'test']
+        fname_list = cfg.train_list if train_or_test == "train" else cfg.test_list
+        self.train_or_test = train_or_test
+        fname_list = [fname_list] if type(fname_list) is not list else fname_list
+
+        self.dp_list = []
+        for fname in fname_list:
+            self.dp_list.extend(get_data_list(fname))
+        self.shuffle = shuffle
+
+    def size(self):
+        return len(self.dp_list)
+
+    def get_data(self):
+        idxs = np.arange(len(self.dp_list))
+        if self.shuffle:
+            self.rng.shuffle(idxs)
+        for k in idxs:
+            dp = self.dp_list[k]
+            img_path = dp[0]
+            img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+            bone = dp[1:]
+            bone = np.asarray([float(ele) for ele in bone])
+            bone = bone - cfg.anchor_bones
+            yield [img, bone]
+
+
+if __name__ == '__main__':
+    ds = Data('sealion', 'train')
